0c6ef31f8e8ae625e41caca8a743fcf6d8f75b05
JoseMMTW99 <josemmtw99@gmail.com>
2024-09-15 22:14:33 -0300

    node src/server.js funcionando

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,25 @@
 const path = require('path');
 const dotenv = require('dotenv');
-const { connect } = require('mongoose');
 const program = require('../utils/commander');
 const MongoSingleton = require('../utils/MongoSingleton');
 
 const { mode } = program.opts();
+console.log('Mode:', mode);  // Debugging line to check the value of mode
+
+// Cargar el archivo .env por defecto
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// Cargar el archivo de entorno específico si se ha especificado un modo
+if (mode) {
+    const envFilePath = path.resolve(__dirname, `../.env.${mode}`);
+    console.log('Loading env file:', envFilePath);  // Debugging line to check the file path
+    dotenv.config({ path: envFilePath });
+}
 
 const objectConfig = {
     port: process.env.PORT || 8080,
     mongo_url: process.env.MONGO_URL,
-    jwt_private_key: process.env.JWT_PRIVATE_KEY ,
+    jwt_private_key: process.env.JWT_PRIVATE_KEY,
     persistence: process.env.PERSISTENCE,
     gmail_user: process.env.GMAIL_USER,
     gmail_pass: process.env.GMAIL_PASS,
@@ -19,16 +28,15 @@ const objectConfig = {
     twilio_phone: process.env.TWILIO_PHONE,
     stripe_key: process.env.STRIPE_KEY,
     stripe_secret_key: process.env.STRIPE_SECRET_KEY
-}
+};
 
-console.log('Stripe Key:', process.env.STRIPE_KEY);
-console.log('Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
+console.log('Port:', objectConfig.port);  // Debugging line to check the loaded port
 
 const connectDB = async () => {
-    MongoSingleton.getInstance()
-}
+    await MongoSingleton.getInstance();
+};
 
 module.exports = {
     objectConfig,
     connectDB
-}
+};
